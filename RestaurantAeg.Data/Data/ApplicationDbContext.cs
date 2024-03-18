e2528fca21349e23413f9b24c106e70db3430108
peshopbs2 <peshopbs2@gmail.com>
2024-03-18 05:28:30 -0800

    scaffold categories controller

--- RestaurantAeg.Data/Data/ApplicationDbContext.cs
+++ RestaurantAeg.Data/Data/ApplicationDbContext.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using RestaurantAeg.Data.Entities;
 
 namespace RestaurantAeg.Data
 {
@@ -8,6 +11,34 @@ namespace RestaurantAeg.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<RestaurantCategory> RestaurantCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<RestaurantCategory>()
+                .HasKey(rc => new { rc.RestaurantId, rc.CategoryId });
+
+            base.OnModelCreating(builder);
+        }
+    }
+
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+            return new ApplicationDbContext(builder.Options);
         }
     }
 }
